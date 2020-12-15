36a00dc89b7d3e2651e24525be89f61770653e7f
Benjamin Morrison <ben@gbmor.dev>
2020-12-15 13:48:31 -0500

    the array was unnecessary

--- day15/main.go
+++ day15/main.go
@@ -12,29 +12,24 @@ func getInput(test int) []int {
 	return []int{5, 1, 9, 18, 13, 8, 0}
 }
 
-type Pos struct {
-	last, prev int
-}
-
 func exec(s []int, breaker int) int {
-	d := make([]int, breaker)
-	copy(d, s)
 	cache := make(map[int]int)
 	for i, e := range s {
 		cache[e] = i
 	}
+	current := 0
 	for i := len(s); i < breaker-1; i++ {
-		if _, ok := cache[d[i]]; !ok {
-			d[i+1] = 0
-			cache[d[i]] = i
+		if _, ok := cache[current]; !ok {
+			cache[current] = i
+			current = 0
 			continue
 		}
-		pos := cache[d[i]]
+		pos := cache[current]
 		diff := i - pos
-		cache[d[i]] = i
-		d[i+1] = diff
+		cache[current] = i
+		current = diff
 	}
-	return d[len(d)-1]
+	return current
 }
 
 func main() {
